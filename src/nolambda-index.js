7f435664babff3b0e46011e620ff6f3b0889d151
Gaizka <gaizkav@gmail.com>
2019-04-27 22:27:31 +0200

    Build an index.js that will work for node regular apps
    Existing index.ts works only for probot-serverless-now apps

---
+++ src/nolambda-index.js
@@ -0,0 +1,58 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const autorebase_1 = require("./autorebase");
+const requireWriteAccessForOneTimeRebase = permission => permission === "admin" || permission === "write";
+exports.requireWriteAccessForOneTimeRebase = requireWriteAccessForOneTimeRebase;
+
+const nopHandler = () => Promise.resolve();
+
+var options =  {
+  canRebaseOneTime: requireWriteAccessForOneTimeRebase,
+     handleAction: nopHandler,
+     handleEvent: nopHandler,
+     label: "autorebase"
+};
+
+module.exports = app => {
+      app.log("App loaded");
+    app.on([
+        "check_run.completed",
+        "issue_comment.created",
+        "pull_request",
+        "pull_request_review.submitted",
+        "status",
+    ], async (context) => {
+        const { owner, repo } = context.repo();
+        const event = {
+            id: context.id,
+            // @ts-ignore The event is of the good type because Autorebase only subscribes to a subset of webhooks.
+            name: context.name,
+            payload: context.payload,
+        };
+        const handlerResult = await options.handleEvent(event);
+        const forceRebase = handlerResult === true;
+        let action = { type: "nop" };
+        try {
+            action = await autorebase_1.autorebase({
+                canRebaseOneTime: options.canRebaseOneTime,
+                event,
+                forceRebase,
+                label: options.label,
+                // The value is the good one even if the type doesn't match.
+                octokit: context.github,
+                owner,
+                repo,
+            });
+        }
+        catch (error) {
+            action = { error, type: "failed" };
+            throw error;
+        }
+        finally {
+            context.log(action);
+            if (action.type !== "nop") {
+                await options.handleAction(action);
+            }
+        }
+    });
+};
